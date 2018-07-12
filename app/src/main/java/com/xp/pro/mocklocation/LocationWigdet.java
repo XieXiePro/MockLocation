737facb2a043687c2b8515aaf0aede9499b39787
XieXiePro <626993952@qq.com>
2018-07-12 22:37:18 +0800

    first commit

---
+++ app/src/main/java/com/xp/pro/mocklocation/LocationWigdet.java
@@ -0,0 +1,13 @@
+package com.xp.pro.mocklocation;
+
+/**
+ * LocationWigdet:模拟位置信息提示控件
+ * Author: xp
+ * Date: 18/7/12 22:22
+ * Email: xiexiepro@gmail.com
+ * Blog: http://XieXiePro.github.io
+ */
+public class LocationWigdet {
+
+
+}
